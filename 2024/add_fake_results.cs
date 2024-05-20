31b5fdd8c682973fcda181a142ba9c6ac13fa4b6
Tim Reynolds <tim@cubingusa.org>
2024-05-20 08:10:42 -0400

    For fake results, only do top N in multi / FMC.
    This is all temporary until registration closes anyway.

--- 2024/add_fake_results.cs
+++ 2024/add_fake_results.cs
@@ -1,2 +1,8 @@
 # For dev, add all people to the round.
-Map(Events(), AddResults(RoundForEvent(1), Persons(CompetingIn())))
+Map(Events(),
+    AddResults(
+        RoundForEvent(1),
+        Persons(
+            And(CompetingIn(),
+                (PsychSheetPosition() <=
+                 Switch(Arg<Event>(), [Tuple(_333mbf, 20), Tuple(_333fm, 30)], 99999))))))
