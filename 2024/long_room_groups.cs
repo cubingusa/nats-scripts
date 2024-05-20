62449758ea91d15af49b7e62a45004f7db3c4906
Tim Reynolds <tim@cubingusa.org>
2024-05-20 08:11:44 -0400

    Add a script to create log room groups.

---
+++ 2024/long_room_groups.cs
@@ -0,0 +1,8 @@
+#include "lib/_assignment_sets.cs"
+
+AssignGroups(_333mbf-r1, EveryoneAssignmentSets(), attemptNumber=1)
+AssignGroups(_333mbf-r1, EveryoneAssignmentSets(), attemptNumber=2)
+AssignGroups(_333fm-r1, EveryoneAssignmentSets(), attemptNumber=1)
+AssignGroups(_333fm-r1, EveryoneAssignmentSets(), attemptNumber=2)
+AssignGroups(_333fm-r1, EveryoneAssignmentSets(), attemptNumber=3)
+AssignGroups(_555bf-r1, EveryoneAssignmentSets())
