ce9a541649a5b294bc3baf288d76e938a84cd482
Tim Reynolds <tim@cubingusa.org>
2024-06-02 18:18:30 -0400

    Add all unregistered staff.

--- 2024/prep/add_noncompeting_staff.cs
+++ 2024/prep/add_noncompeting_staff.cs
@@ -4,3 +4,15 @@ AddPerson(78419) # Nancy Hartman
 AddPerson(385123) # Wendy Nieuwland
 AddPerson(40580) # Peter Still
 AddPerson(282837) # Megan Fedders
+# TODO: Connie Workman needs a WCA account.
+
+# The following staff are expected to compete, but have not registered yet.
+AddPerson(186300) # Draco Tong
+AddPerson(287959) # Rex Kotovich
+AddPerson(137800) # Stevie Shogren
+AddPerson(73) # Saransh Grover
+AddPerson(354017) # Cameron Petursson
+AddPerson(5539) # Jesse Mailloux
+AddPerson(44301) # Chris Tabar
+AddPerson(13545) # Chris Martin
+AddPerson(125561) # Cade Beck
