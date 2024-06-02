a6a71711aa059b926f7fb68369970313e428c418
Tim Reynolds <tim@cubingusa.org>
2024-06-02 18:03:12 -0400

    Use Wendy's new WCA account.

--- 2024/prep/add_noncompeting_staff.cs
+++ 2024/prep/add_noncompeting_staff.cs
@@ -1,6 +1,6 @@
 AddPerson(340272) # Aaron Mack
 AddPerson(405027) # John Wanger
 AddPerson(78419) # Nancy Hartman
-AddPerson(191476) # Wendy Nieuwland
+AddPerson(385123) # Wendy Nieuwland
 AddPerson(40580) # Peter Still
 AddPerson(282837) # Megan Fedders
