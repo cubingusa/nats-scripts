49f81f0a8a5d3cd7d2745dd7a17ebc9628335b5c
Tim Reynolds <tim@cubingusa.org>
2024-06-03 08:13:22 -0400

    Add James to multi attempt 2 staff.

--- 2024/lib/_long_room_staff.cs
+++ 2024/lib/_long_room_staff.cs
@@ -2,7 +2,7 @@
 
 Define("StaffingThursdayMulti", In(NumberProperty(LONG_ROOM_TEAM), [1, 2]))
 Define("StaffingThursdayFMC", In(NumberProperty(LONG_ROOM_TEAM), [1]))
-Define("StaffingFridayMulti", In(NumberProperty(LONG_ROOM_TEAM), [1, 2]))
+Define("StaffingFridayMulti", Or(In(NumberProperty(LONG_ROOM_TEAM), [1, 2]), (Name() == "James Hildreth")))
 Define("StaffingFridayFMC", In(NumberProperty(LONG_ROOM_TEAM), [2]))
 Define("StaffingSaturday5BLD", In(NumberProperty(LONG_ROOM_TEAM), [1, 2, 3]))
 Define("StaffingSaturdayFMC", In(NumberProperty(LONG_ROOM_TEAM), [1]))
