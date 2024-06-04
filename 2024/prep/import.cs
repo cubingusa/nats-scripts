69e5f76ba9d928947e557dfcda0b3476b7f38b43
Tim Reynolds <tim@cubingusa.org>
2024-06-04 08:15:03 -0400

    Set the right property for preferences.
    Fixes #2

--- 2024/prep/import.cs
+++ 2024/prep/import.cs
@@ -1,6 +1,6 @@
 #include "../lib/_constants.cs"
 
-ReadSpreadsheet("1-KNvZD5o6A4oeqwypppvSwgMnpLMjElo6B-kPvr4dco", offset=1, sheetTitle="Preferences")
+ReadSpreadsheet("1-KNvZD5o6A4oeqwypppvSwgMnpLMjElo6B-kPvr4dco", offset=1, sheetTitle="percents")
 
 # Set some derived properties.
 DeleteRole(Persons(HasRole(DATAENTRY)), DATAENTRY)
@@ -30,12 +30,12 @@ SetProperty(
         (NumberProperty("attending") > 2)
       )
     ), COMMENTATOR, true)
-SetProperty(Persons(BooleanProperty(COMMENTATOR)), "preference-Commentator", 100)
+SetProperty(Persons(BooleanProperty(COMMENTATOR)), "percent-Commentator", 100)
 
 DeleteProperty(Persons(HasProperty(STAGE_LEAD)), STAGE_LEAD)
 SetProperty([2010AMBR01, 2008YOUN02, 2013DICK01, 2014NIEL03, 2020LONG05, 2017WOFF01, 2015COHE02, 2015ROYC01],
             STAGE_LEAD, true)
-SetProperty(Persons(BooleanProperty(STAGE_LEAD)), "preference-Delegate", 100)
+SetProperty(Persons(BooleanProperty(STAGE_LEAD)), "percent-Delegate", 100)
 
 DeleteProperty(Persons(HasProperty(WCA_BOARD)), WCA_BOARD)
 SetProperty(Filter([2014PRID01, 2010THOM03, 2019JARM01, 2016CHAI03, 2016SILV08], Not(IsNull(Arg<Person>()))), WCA_BOARD, true)
