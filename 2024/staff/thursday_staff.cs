32222c6717d72f1fb658938562f68a22771e5e8f
Tim Reynolds <tim@cubingusa.org>
2024-06-04 07:30:08 -0400

    Adjust scrambler count.

--- 2024/staff/thursday_staff.cs
+++ 2024/staff/thursday_staff.cs
@@ -9,7 +9,7 @@ DoStaffAssignments(_sq1-r1, 2024-07-18,
                    [Judges(), Scramblers(3, _sq1), Runners(4, _sq1), Delegate()],
                    StaffScorers(_sq1))
 DoStaffAssignments(_555-r1, 2024-07-18,
-                   [Judges(), Scramblers(3, _555), Runners(4, _555), Delegate()],
+                   [Judges(), Scramblers(4, _555), Runners(3, _555), Delegate()],
                    StaffScorers(_555))
 DoStaffAssignments(_clock-r1, 2024-07-18,
                    [Judges(), Scramblers(3, _clock), Runners(4, _clock), Delegate()],
