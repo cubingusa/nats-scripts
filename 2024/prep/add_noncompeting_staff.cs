7ea1b1685769784161c24dccc28d3dff8ab952b5
Tim Reynolds <tim@cubingusa.org>
2024-06-06 08:20:17 -0400

    Remove recently-registered staff.

--- 2024/prep/add_noncompeting_staff.cs
+++ 2024/prep/add_noncompeting_staff.cs
@@ -9,10 +9,7 @@ AddPerson(282837) # Megan Fedders
 # The following staff are expected to compete, but have not registered yet.
 AddPerson(186300) # Draco Tong
 AddPerson(287959) # Rex Kotovich
-AddPerson(137800) # Stevie Shogren
 AddPerson(73) # Saransh Grover
 AddPerson(354017) # Cameron Petursson
-AddPerson(5539) # Jesse Mailloux
-AddPerson(44301) # Chris Tabar
 AddPerson(13545) # Chris Martin
 AddPerson(125561) # Cade Beck
