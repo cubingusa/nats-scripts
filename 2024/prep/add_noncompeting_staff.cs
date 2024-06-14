fb5164460487abe827ff0e789567180fa3f674dc
Tim Reynolds <tim@cubingusa.org>
2024-06-13 22:03:23 -0400

    correction: Rex Kotovich is not registered.

--- 2024/prep/add_noncompeting_staff.cs
+++ 2024/prep/add_noncompeting_staff.cs
@@ -4,4 +4,5 @@ AddPerson(78419) # Nancy Hartman
 AddPerson(385123) # Wendy Nieuwland
 AddPerson(40580) # Peter Still
 AddPerson(282837) # Megan Fedders
+AddPerson(287959) # Rex Kotovich
 # TODO: Connie Workman needs a WCA account.
