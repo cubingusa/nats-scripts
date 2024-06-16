47363c7cc879352ea6de6ae5340db36d175ed169
Tim Reynolds <tim@cubingusa.org>
2024-06-16 10:57:03 -0400

    Add file for special CompetitionGroups staff.

---
+++ 2024/staff/competition_groups_staff.cs
@@ -0,0 +1,25 @@
+#include "../lib/_long_room_staff.cs"
+
+ClearCompetitionGroupsAssignments(Persons(true))
+
+CreateCompetitionGroupsAssignments(
+    Persons(StaffingThursdayMulti(Arg<Person>())),
+    "Scramble Multi Blind", 2024-07-18T10:00, 2024-07-18T12:00)
+ 
+CreateCompetitionGroupsAssignments(
+    Persons(StaffingFridayMulti(Arg<Person>())),
+    "Scramble Multi Blind", 2024-07-19T10:00, 2024-07-19T12:00)
+
+CreateCompetitionGroupsAssignments(
+    [2018KHAN26, 2018KHAN27, 2018KHAN28, 2014GULA02],
+    "Compete in OH (Blue Stage)", 2024-07-18T08:30, 2024-07-18T09:00)
+CreateCompetitionGroupsAssignments(
+    [2008YOUN02, 2017WOFF01],
+    "Accommodation for OH (Blue Stage)", 2024-07-18T08:30, 2024-07-18T09:00)
+
+CreateCompetitionGroupsAssignments(
+    [2015PADG01, 2024BOIS01],
+    "Compete in Saturday events (Blue Stage)", 2024-07-19T12:30, 2024-07-19T13:15)
+CreateCompetitionGroupsAssignments(
+    [2010AMBR01, 2020LONG05],
+    "Accommodation for Saturday events (Blue Stage)", 2024-07-19T12:30, 2024-07-19T13:15)
