51f81f4bba94a351395b7dcddadb645bfc484c4e
Tim Reynolds <tim@cubingusa.org>
2024-06-17 20:13:22 -0400

    Show multi drop-off on the schedule.

--- 2024/staff/competition_groups_staff.cs
+++ 2024/staff/competition_groups_staff.cs
@@ -1,3 +1,4 @@
+#include "../lib/_constants.cs"
 #include "../lib/_long_room_staff.cs"
 
 ClearCompetitionGroupsAssignments(Persons(true))
@@ -23,3 +24,17 @@ CreateCompetitionGroupsAssignments(
 CreateCompetitionGroupsAssignments(
     [2010AMBR01, 2020LONG05],
     "Accommodation for Saturday events (Blue Stage)", 2024-07-19T12:30, 2024-07-19T13:15)
+
+CreateCompetitionGroupsAssignments(
+    Persons(And(CompetingIn(_333mbf), Not(IsStaff(Arg<Person>())))),
+    "Multi-Blind Cube Submission", 2024-07-18T09:30, 2024-07-18T10:00)
+CreateCompetitionGroupsAssignments(
+    Persons(And(CompetingIn(_333mbf), Not(IsStaff(Arg<Person>())))),
+    "Multi-Blind Cube Submission", 2024-07-19T09:30, 2024-07-19T10:00)
+
+CreateCompetitionGroupsAssignments(
+    Concat([2014GULA02], Persons(And(CompetingIn(_333mbf), IsStaff(Arg<Person>())))),
+    "Multi-Blind Cube Submission", 2024-07-18T08:50, 2024-07-18T10:00)
+CreateCompetitionGroupsAssignments(
+    Concat([2014GULA02], Persons(And(CompetingIn(_333mbf), IsStaff(Arg<Person>())))),
+    "Multi-Blind Cube Submission", 2024-07-19T08:30, 2024-07-19T10:00)
