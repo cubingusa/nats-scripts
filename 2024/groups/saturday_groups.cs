f6ee781e46dab0f48ab2c62f3a84b5975b91f4bc
Tim Reynolds <tim@cubingusa.org>
2024-06-22 07:33:12 -0400

    Manually assign Livia and Timothy Huynh's groups.
    They are participating in a youtube event.

--- 2024/groups/saturday_groups.cs
+++ 2024/groups/saturday_groups.cs
@@ -16,7 +16,8 @@ AssignGroups(_333-r1,
                      ByFilters(BooleanProperty(WCA_BOARD), (GroupNumber() == 1), 10),
                      ByFilters((Arg<Person>() == 2020LONG05), (GroupNumber() == 1), 10),
                      ByFilters((Arg<Person>() == 2010AMBR01), (GroupNumber() == 2), 10),
-                     ByFilters((Arg<Person>() == 2018AMSB02), (GroupNumber() < 7), 10)]))
+                     ByFilters((Arg<Person>() == 2018AMSB02), (GroupNumber() < 7), 10),
+                     ByFilters(In(Arg<Person>(), [2013KLEI03, 2017HUYN02]), (EndTime() < 2024-07-20T10:30), 10)]))
 
 AssignGroups(_222-r1,
              RoundOneAssignmentSets(_222, 2024-07-20),
@@ -27,7 +28,9 @@ AssignGroups(_222-r1,
                      ByFilters(Or(CompetingIn(_333fm),
                                   StaffingSaturdayFMC(Arg<Person>()),
                                   BooleanProperty(STAGE_LEAD)), FMCOverlap(Arg<Group>()), -20),
-                     ByFilters(BooleanProperty(WCA_BOARD), (GroupNumber() == 12), 10)]))
+                     ByFilters(BooleanProperty(WCA_BOARD), (GroupNumber() == 12), 10),
+                     ByFilters((Arg<Person>() == 2013KLEI03), (GroupNumber() == 1), 10),
+                     ByFilters((Arg<Person>() == 2017HUYN02), (GroupNumber() == 12), 10)]))
 
 AssignGroups(_skewb-r1,
              RoundOneAssignmentSets(_skewb, 2024-07-20),
@@ -35,4 +38,5 @@ AssignGroups(_skewb-r1,
                     [ByFilters(Or(CompetingIn(_333fm),
                                   StaffingSaturdayFMC(Arg<Person>()),
                                   BooleanProperty(STAGE_LEAD)), FMCOverlap(Arg<Group>()), -20),
-                     ByFilters(BooleanProperty(WCA_BOARD), (GroupNumber() == 1), 10)]))
+                     ByFilters(BooleanProperty(WCA_BOARD), (GroupNumber() == 1), 10),
+                     ByFilters(In(Arg<Person>(), [2013KLEI03, 2017HUYN02]), (StartTime() > 2024-07-20T16:15), 10)]))
