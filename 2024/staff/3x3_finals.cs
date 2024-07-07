312cc592a814d223f771808083e3cf8e1746a861
Tim Reynolds <tim@cubingusa.org>
2024-07-07 12:30:20 -0400

    Add 3x3 finals staff assignments.

---
+++ 2024/staff/3x3_finals.cs
@@ -0,0 +1,10 @@
+#include "../lib/_constants.cs"
+
+Map([1, 2, 3, 4, 5, 6, 7, 8],
+    All(
+      ManuallyAssign([2009HILD01], _333-r4, RED, Arg<Number>(), "judge"),
+      ManuallyAssign([2011WELC01], _333-r4, BLUE, Arg<Number>(), "judge"),
+      ManuallyAssign([2008YOUN02], _333-r4, RED, Arg<Number>(), "scrambler"),
+      ManuallyAssign([2010AMBR01], _333-r4, BLUE, Arg<Number>(), "scrambler"),
+      ManuallyAssign([2004CHAN04], _333-r4, RED, Arg<Number>(), "runner"),
+      ManuallyAssign([2013DICK01], _333-r4, BLUE, Arg<Number>(), "runner")))
