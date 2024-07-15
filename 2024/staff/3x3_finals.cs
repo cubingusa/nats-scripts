537cdbb6f318155d302fbc27504f50fee16774a8
Tim Reynolds <tim@cubingusa.org>
2024-07-15 14:45:02 -0500

    Fix finals assignment.

--- 2024/staff/3x3_finals.cs
+++ 2024/staff/3x3_finals.cs
@@ -2,9 +2,9 @@
 
 Map([1, 2, 3, 4, 5, 6, 7, 8],
     All(
-      ManuallyAssign([2009HILD01], _333-r4, RED, Arg<Number>(), "judge"),
-      ManuallyAssign([2011WELC01], _333-r4, BLUE, Arg<Number>(), "judge"),
-      ManuallyAssign([2008YOUN02], _333-r4, RED, Arg<Number>(), "scrambler"),
-      ManuallyAssign([2010AMBR01], _333-r4, BLUE, Arg<Number>(), "scrambler"),
-      ManuallyAssign([2004CHAN04], _333-r4, RED, Arg<Number>(), "runner"),
-      ManuallyAssign([2013DICK01], _333-r4, BLUE, Arg<Number>(), "runner")))
+      ManuallyAssign([2009HILD01], _333-r4, RED, Arg<Number>(), "staff-judge"),
+      ManuallyAssign([2011WELC01], _333-r4, BLUE, Arg<Number>(), "staff-judge"),
+      ManuallyAssign([2008YOUN02], _333-r4, RED, Arg<Number>(), "staff-scrambler"),
+      ManuallyAssign([2010AMBR01], _333-r4, BLUE, Arg<Number>(), "staff-scrambler"),
+      ManuallyAssign([2004CHAN04], _333-r4, RED, Arg<Number>(), "staff-runner"),
+      ManuallyAssign([2013DICK01], _333-r4, BLUE, Arg<Number>(), "staff-runner")))
