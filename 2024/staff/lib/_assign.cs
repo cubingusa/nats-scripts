d44b36b7a36a9635916aac88d7272c2b3e9d1112
Tim Reynolds <tim@cubingusa.org>
2024-07-15 14:44:41 -0500

    Undo stray changes.

--- 2024/staff/lib/_assign.cs
+++ 2024/staff/lib/_assign.cs
@@ -9,11 +9,11 @@
 # 3: Jobs (see _jobs.cs)
 # 4: Scorers (see _scorers.cs)
 Define("DoStaffAssignments",
-       Map([ORANGE],
+       Map(Stages(),
            AssignStaff(
              {1, Round},
              (Stage() == Arg<String>()),
              Persons((AssignedStage(Arg<Person>(), {2, Date}) == Arg<String>())),
              {3, Array<AssignmentJob>},
              {4, Array<AssignmentScorer>},
-             unavailable=Unavailable(Arg<Person>()), overwrite=true)))
+             unavailable=Unavailable(Arg<Person>()))))
