4e82876b652017ed7ad179d62bf84b9c94d6762e
Tim Reynolds <tim@cubingusa.org>
2024-07-18 13:56:25 -0500

    Add extra scramblers for midcomp_1.

--- 2024/staff/midcomp_1.cs
+++ 2024/staff/midcomp_1.cs
@@ -3,11 +3,11 @@
 #include "lib/_scorers.cs"
 
 DoStaffAssignments(_minx-r2, 2024-07-18,
-                   [Judges(), Scramblers(4, _minx), Runners(3, _minx), Delegate()],
+                   [Judges(), Scramblers(5, _minx), Runners(3, _minx), Delegate()],
                    StaffScorers(_minx, 2024-07-18))
 DoStaffAssignments(_sq1-r2, 2024-07-18,
-                   [Judges(), Scramblers(3, _sq1), Runners(4, _sq1), Delegate()],
+                   [Judges(), Scramblers(4, _sq1), Runners(4, _sq1), Delegate()],
                    StaffScorers(_sq1, 2024-07-18))
 DoStaffAssignments(_555-r2, 2024-07-18,
-                   [Judges(), Scramblers(4, _555), Runners(3, _555), Delegate()],
+                   [Judges(), Scramblers(5, _555), Runners(3, _555), Delegate()],
                    StaffScorers(_555, 2024-07-18))
