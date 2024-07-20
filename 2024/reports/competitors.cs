8b22834680e90cd4d4e6b20d0227488b097b39d4
Tim Reynolds <tim@cubingusa.org>
2024-07-20 16:07:10 -0500

    Update competitors report.

--- 2024/reports/competitors.cs
+++ 2024/reports/competitors.cs
@@ -1,5 +1,8 @@
-"Number Competed"
+"Number competed"
 Length(Persons(HasResults()))
 ""
-"Likely No-Shows"
+"Likely no-shows"
 Length(Persons(IsPossibleNoShow()))
+""
+"First event hasn't happened yet"
+(Length(Persons(Registered())) - (Length(Persons(IsPossibleNoShow())) + Length(Persons(HasResults()))))
