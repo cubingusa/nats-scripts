09ac6c70fcaa41f6ca4714a78792219018fd7c3a
Tim Reynolds <tim@cubingusa.org>
2024-07-22 20:16:44 -0500

    Change finals staff.

--- 2024/staff/3x3_finals.cs
+++ 2024/staff/3x3_finals.cs
@@ -1,6 +1,15 @@
 #include "../lib/_constants.cs"
 
-Map([1, 2, 3, 4, 5, 6, 7, 8],
+Map([1, 2, 3, 4],
+    All(
+      ManuallyAssign([2013DICK01], _333-r4, BLUE, Arg<Number>(), "staff-runner"),
+      ManuallyAssign([2009HILD01], _333-r4, RED, Arg<Number>(), "staff-runner"),
+      ManuallyAssign([2011WELC01], _333-r4, BLUE, Arg<Number>(), "staff-scrambler"),
+      ManuallyAssign([2004CHAN04], _333-r4, RED, Arg<Number>(), "staff-scrambler"),
+      ManuallyAssign([2008YOUN02], _333-r4, RED, Arg<Number>(), "staff-judge"),
+      ManuallyAssign([2010AMBR01], _333-r4, BLUE, Arg<Number>(), "staff-judge")))
+
+Map([5, 6, 7, 8],
     All(
       ManuallyAssign([2009HILD01], _333-r4, RED, Arg<Number>(), "staff-judge"),
       ManuallyAssign([2011WELC01], _333-r4, BLUE, Arg<Number>(), "staff-judge"),
