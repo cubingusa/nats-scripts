8a9d621181a881791ece046ecb0174fdf7983c99
Tim Reynolds <tim@cubingusa.org>
2024-12-21 16:58:01 -0500

    Prefer some country overlap.

--- 2025/groups/lib/_scorers.cs
+++ 2025/groups/lib/_scorers.cs
@@ -1,8 +1,8 @@
 # TODO: First name clusters
 # TODO: Staff stages
 # TODO: add time between competing in different rooms.
-# TODO: Limit max value of matching countries.
 
 Define("DefaultScorers",
        [ByMatchingValue(FirstName(), -5),
-        ByMatchingValue(Country(), 10)])
+        ByMatchingValue(Country(), 4, limit=2),
+        ByMatchingValue(Country(), -1)])
