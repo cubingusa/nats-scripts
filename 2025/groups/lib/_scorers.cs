34724b27d245030cc6fdda0dfec3446c5de7f28a
Tim Reynolds <tim@cubingusa.org>
2024-12-21 21:04:43 -0500

    Add scorer preferring 2^n+1.
    This spreads out assignment sets that don't evenly fill groups, e.g.
    small assignment sets like stage leads.
    
    Fixes #12.

--- 2025/groups/lib/_scorers.cs
+++ 2025/groups/lib/_scorers.cs
@@ -5,4 +5,8 @@
 Define("DefaultScorers",
        [ByMatchingValue(FirstName(), -5),
         ByMatchingValue(Country(), 4, limit=2),
-        ByMatchingValue(Country(), -1)])
+        ByMatchingValue(Country(), -1),
+        # Prefer groups 2^k+1. This makes sure that smaller AssignmentSets are effectively spread out, rather than e.g. putting all of the stage leads together.
+        ByFilters(true, (Mod(GroupNumber(), 2) == 1), 1),
+        ByFilters(true, (Mod(GroupNumber(), 4) == 1), 1),
+        ByFilters(true, (Mod(GroupNumber(), 8) == 1), 1)])
