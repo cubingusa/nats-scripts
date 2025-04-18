88ebb38e3da80fd16e7c9d46fca132d590eecc54
Tim Reynolds <tim@cubingusa.org>
2025-04-17 21:17:53 -0400

    Put faster volunteers on stages in the main hall for that event.

--- 2025/prep/volunteer_teams.cs
+++ 2025/prep/volunteer_teams.cs
@@ -1,5 +1,7 @@
+#include "../lib/_assigned_stage.cs"
 #include "../lib/_constants.cs"
 #include "../lib/_eligible_scramblers.cs"
+#include "../lib/_group_counts.cs"
 
 #define LONG_ROOM_PREFERENCE "long-room-preference"
 #define LONG_ROOM_YES "Yes please! As much as possible"
@@ -69,6 +71,9 @@ Define(
     Map([_333, _222, _444, _555, _666, _777, _333bf, _333oh, _clock, _pyram, _skewb, _sq1, _minx],
       BalanceConstraint((EventId() + "-semi"), (PsychSheetPosition() < 200), 2)))
 
+Define("NPerGroup",
+       (PsychSheetPosition({1, Event}) < ({2, Number} * {3, Number})))
+
 Define(
     "SpecificTeams",
     [
@@ -76,9 +81,67 @@ Define(
         "ChineseTeams",
         In(Country(), ["CN", "HK", "TW"]),
         (Arg<Number>() <= 3),
-        50
-      )
-    ])
+        50),
+      SpecificAssignmentScore(
+        "ThursdayStrongPreference",
+        Or(NPerGroup(_clock, MAIN_CLOCK, 8),
+           NPerGroup(_555, MAIN_555, 8),
+           NPerGroup(_333oh, MAIN_333OH, 8),
+           NPerGroup(_pyram, MAIN_PYRAM, 8)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-03), MainStages()),
+        50),
+      SpecificAssignmentScore(
+        "ThursdayWeakPreference",
+        Or(NPerGroup(_clock, MAIN_CLOCK, 20),
+           NPerGroup(_555, MAIN_555, 20),
+           NPerGroup(_333oh, MAIN_333OH, 20),
+           NPerGroup(_pyram, MAIN_PYRAM, 20)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-03), MainStages()),
+        30),
+      SpecificAssignmentScore(
+        "FridayStrongPreference",
+        Or(NPerGroup(_777, MAIN_777, 8),
+           NPerGroup(_222, MAIN_222, 8),
+           NPerGroup(_444, MAIN_444, 8),
+           NPerGroup(_minx, MAIN_MINX, 8)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-04), MainStages()),
+        50),
+      SpecificAssignmentScore(
+        "FridayWeakPreference",
+        Or(NPerGroup(_777, MAIN_777, 20),
+           NPerGroup(_222, MAIN_222, 20),
+           NPerGroup(_444, MAIN_444, 20),
+           NPerGroup(_minx, MAIN_MINX, 20)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-04), MainStages()),
+        30),
+      SpecificAssignmentScore(
+        "SaturdayStrongPreference",
+        Or(NPerGroup(_333bf, MAIN_333BF, 8),
+           NPerGroup(_skewb, MAIN_SKEWB, 8),
+           NPerGroup(_333, MAIN_333, 8),
+           NPerGroup(_666, MAIN_666, 8)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-05), MainStages()),
+        50),
+      SpecificAssignmentScore(
+        "SaturdayWeakPreference",
+        Or(NPerGroup(_333bf, MAIN_333BF, 20),
+           NPerGroup(_skewb, MAIN_SKEWB, 20),
+           NPerGroup(_333, MAIN_333, 20),
+           NPerGroup(_666, MAIN_666, 20)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-05), MainStages()),
+        30),
+      SpecificAssignmentScore(
+        "SundayStrongPreference",
+        Or(NPerGroup(_444bf, 1, 12),
+           NPerGroup(_sq1, MAIN_SQ1, 8)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-06), MainStages()),
+        50),
+      SpecificAssignmentScore(
+        "SundayWeakPreference",
+        Or(NPerGroup(_444bf, 1, 20),
+           NPerGroup(_sq1, MAIN_SQ1, 20)),
+        In(AssignedStageForTeam(Arg<Number>(), 2025-07-06), MainStages()),
+        30)])
 
 DeleteProperty(Persons(HasProperty(STAFF_TEAM)), STAFF_TEAM)
 
