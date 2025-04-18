5acf1ab6bfe77ebdef11b4fb7f381223285b34fd
Tim Reynolds <tim@cubingusa.org>
2025-04-17 21:26:26 -0400

    Chinese-speaking competitors matched with stage leads.

--- 2025/groups/lib/_scorers.cs
+++ 2025/groups/lib/_scorers.cs
@@ -6,6 +6,9 @@ Define("DefaultScorers",
        [ByMatchingValue(FirstName(), -5),
         ByMatchingValue(Country(), 4, limit=2),
         ByMatchingValue(Country(), -1),
+        # Put competitors in Chinese-speaking countries on one of the stages with a Chinese-speaking stage lead.
+        ByFilters(In(Country(), ["CN", "HK", "TW", "SG"]),
+                  In(Stage(), [MAIN_RED, MAIN_GREEN, SIDE_RED]), 10),
         # Prefer groups 2^k+1. This makes sure that smaller AssignmentSets are effectively spread out, rather than e.g. putting all of the stage leads together.
         ByFilters(true, (Mod(GroupNumber(), 2) == 1), 1),
         ByFilters(true, (Mod(GroupNumber(), 4) == 1), 1),
