99ed53689876ee79fd6c45bbb6ce3ae2f4a3f467
Tim Reynolds <tim@cubingusa.org>
2025-05-10 21:35:49 -0400

    Balance unavailable.

--- 2025/prep/volunteer_teams.cs
+++ 2025/prep/volunteer_teams.cs
@@ -31,6 +31,7 @@ Define(
       LimitConstraint("Long Room maybe", IsLongRoomMaybe(), 3, 10),
       BalanceConstraint("< 18", (Age() < 18), 1),
       BalanceConstraint("delegate", (StringProperty("delegate-status") != "Not a WCA Delegate"), 5)
+      BalanceConstraints("Unavailable", NumberProperty("unavail"), 5),
     ])
 
 Define(
