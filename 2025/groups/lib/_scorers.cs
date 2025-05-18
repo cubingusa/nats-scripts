3093fcf4abbbe1e9c65415e81c02d5a41f7a8f25
Tim Reynolds <tim@cubingusa.org>
2025-05-18 08:19:24 -0400

    Score group assignment by how recently you competed.
    Fixes #15.

--- 2025/groups/lib/_scorers.cs
+++ 2025/groups/lib/_scorers.cs
@@ -1,7 +1,6 @@
 #include "_name_clusters.cs"
 
 # TODO: Staff stages
-# TODO: add time between competing in different rooms.
 
 Define("DefaultScorers",
        [ByMatchingValue(FirstNameCluster(), -5),
@@ -13,4 +12,12 @@ Define("DefaultScorers",
         # Prefer groups 2^k+1. This makes sure that smaller AssignmentSets are effectively spread out, rather than e.g. putting all of the stage leads together.
         ByFilters(true, (Mod(GroupNumber(), 2) == 1), 1),
         ByFilters(true, (Mod(GroupNumber(), 4) == 1), 1),
-        ByFilters(true, (Mod(GroupNumber(), 8) == 1), 1)])
+        ByFilters(true, (Mod(GroupNumber(), 8) == 1), 1),
+        RecentlyCompeted(In(Stage(), MainStages()), In(Stage(), MainStages()),
+                         Min(((Arg<Number>() - 30) / 10), 0)),
+        RecentlyCompeted(In(Stage(), SideStages()), In(Stage(), MainStages()),
+                         Min(((Arg<Number>() - 60) / 10), 0)),
+        RecentlyCompeted(In(Stage(), SideStages()), In(Stage(), SideStages()),
+                         Min(((Arg<Number>() - 30) / 10), 0)),
+        RecentlyCompeted(In(Stage(), MainStages()), In(Stage(), SideStages()),
+                         Min(((Arg<Number>() - 60) / 10), 0))])
