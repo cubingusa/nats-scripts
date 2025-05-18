e03cfa14616476561f5f3de2c5a8095933b8979d
Tim Reynolds <tim@cubingusa.org>
2025-05-18 08:44:13 -0400

    Initial assignment report check-in.
    Needs to be tested and duplicated for the other days.
    
    Fixes #18.

---
+++ 2025/reports/_assignments.cs
@@ -0,0 +1,13 @@
+#include "../lib/_assigned_stage.cs"
+#include "../lib/_constants.cs"
+
+# Args:
+# 1: Stage
+# 2: Date
+Define("AssignmentReportForOneStage",
+       AssignmentReport(
+         Persons((AssignedStage(Arg<Person>(), {2, Date}) == {1, Stage})),
+         Filter(AllGroups(),
+                And((Date(StartTime(Arg<Group>()) == {2, Date}))),
+                (Stage() == {1, Stage})),
+         (Weekday({2, Date}) + (" - " + {1, Stage}))))
