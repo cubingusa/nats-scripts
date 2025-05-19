f686a8532145e49b07eb2be8efa364c570736449
Tim Reynolds <tim@cubingusa.org>
2025-05-18 20:56:48 -0400

    Add a sanity check for clock assignments.

--- 2025/groups/r1/clock.cs
+++ 2025/groups/r1/clock.cs
@@ -5,3 +5,11 @@
 AssignGroups(_clock-r1,
              RoundOneAssignmentSets(_clock, 2025-07-03, MAIN_CLOCK, SIDE_CLOCK),
              DefaultScorers())
+
+
+"Sanity Check - 5BLD competitors"
+Table(
+    Sort(Persons(And(CompetingIn(_555bf), CompetingIn(_clock))), StartTime(AssignedGroup(_clock-r1))),
+    [Column("Name", Name()),
+     Column("Group", ((Stage(AssignedGroup(_clock-r1)) + " ") + ToString(GroupNumber(AssignedGroup(_clock-r1))))),
+     Column("StartTime", StartTime(AssignedGroup(_clock-r1)))])
