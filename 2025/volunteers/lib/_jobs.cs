847007027fe39b01b82b213ae41b8379e75ffbf6
Tim Reynolds <tim@cubingusa.org>
2025-05-20 21:31:33 -0400

    Soften eligibility criteria.

--- 2025/volunteers/lib/_jobs.cs
+++ 2025/volunteers/lib/_jobs.cs
@@ -7,19 +7,19 @@ Define("EligibleJudge", Not(And((WcaId() == "2008YUNS01"),
 Define("Judges", Job("judge", 10,
                      assignStations=true,
                      eligibility=And(EligibleJudge({2, Event}),
-                                     Not(HasProperty(STAGE_LEAD)),
-                                     BooleanProperty(CORE_STAFF))))
+                                     Not(BooleanProperty(STAGE_LEAD)),
+                                     BooleanProperty(CORE_VOLUNTEER))))
 Define("Scramblers", Job("scrambler", {1, Number},
                          eligibility=And(CanScramble({2, Event}),
                                          (NumberProperty("percent-scrambler") > 0),
-                                         Not(HasProperty(STAGE_LEAD)),
-                                         BooleanProperty(CORE_STAFF))))
+                                         Not(BooleanProperty(STAGE_LEAD)),
+                                         BooleanProperty(CORE_VOLUNTEER))))
 Define("Checkers", Job("ScrambleChecker", {1, Number},
-                       eligibility=And(Not(HasProperty(STAGE_LEAD)),
-                                       BooleanProperty(CORE_STAFF))))
+                       eligibility=And(Not(BooleanProperty(STAGE_LEAD)),
+                                       BooleanProperty(CORE_VOLUNTEER))))
 Define("Runners", Job("runner", {1, Number},
                       eligibility=And((NumberProperty("percent-runner") > 0),
-                                      Not(HasProperty(STAGE_LEAD)),
-                                      BooleanProperty(CORE_STAFF))))
+                                      Not(BooleanProperty(STAGE_LEAD)),
+                                      BooleanProperty(CORE_VOLUNTEER))))
 Define("PrimaryDelegate", Job("Delegate", 1, eligibility=BooleanProperty(STAGE_LEAD)))
 Define("SecondaryDelegate", Job("SecondaryDelegate", 1, eligibility=BooleanProperty(STAGE_LEAD)))
