efd1964365b5cf14576cc71bd044008abb59fdf1
Tim Reynolds <tim@cubingusa.org>
2025-05-31 11:50:12 -0400

    Delete obsolete TODO.

--- 2025/groups/lib/_scorers.cs
+++ 2025/groups/lib/_scorers.cs
@@ -2,8 +2,6 @@
 #include "../../lib/_constants.cs"
 #include "_name_clusters.cs"
 
-# TODO: Staff stages
-
 Define("DefaultScorers",
        Concat([ByMatchingValue(FirstNameCluster(), -5),
                ByMatchingValue(Country(), 4, limit=2),
@@ -11,7 +9,7 @@ Define("DefaultScorers",
                # Put competitors in Chinese-speaking countries on one of the stages with a Chinese-speaking stage lead.
                ByFilters(In(Country(), ["CN", "HK", "TW", "SG"]),
                          In(Stage(), [MAIN_RED, MAIN_GREEN, SIDE_RED]), 10),
-               # Prefer groups 2^k+1. This makes sure that smaller AssignmentSets are effectively spread out, rather than e.g. putting all of the stage leads together.
+               # Prefer groups 2^k+1. This makes sure that smaller AssignmentSets are effectively spread out, rather than e.g. putting all of the stage leads back-to-back.
                ByFilters(true, (Mod(GroupNumber(), 2) == 1), 1),
                ByFilters(true, (Mod(GroupNumber(), 4) == 1), 1),
                ByFilters(true, (Mod(GroupNumber(), 8) == 1), 1),
