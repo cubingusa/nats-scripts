1ca8b6fff297b09e84034c747a947d17535c8c0f
Tim Reynolds <tim@cubingusa.org>
2025-06-07 14:48:08 -0400

    Simplify 4bld assignment sets.

--- 2025/groups/r1/444bf.cs
+++ 2025/groups/r1/444bf.cs
@@ -3,5 +3,8 @@
 #include "../lib/_scorers.cs"
 
 AssignGroups(_444bf-r1,
-             RoundOneAssignmentSets(_444bf, 2025-07-06, 1, 1),
+             [TopCompetitors(_444bf, 4),
+              FinalsStagesAssignmentSet(_444bf, 32),
+              MainHallAssignmentSet(_444bf, 32, 96),
+              BallroomAssignmentSet(_444bf, 96)],
              DefaultScorers(2025-07-06))
