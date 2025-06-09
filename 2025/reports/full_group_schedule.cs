57c10d715346631bafea442e1dc3a5b1fd951d50
Tim Reynolds <tim@cubingusa.org>
2025-06-09 07:41:39 -0400

    Add the full group schedule.

---
+++ 2025/reports/full_group_schedule.cs
@@ -0,0 +1,11 @@
+#include "../lib/_constants.cs"
+
+Map(
+    [2025-07-03, 2025-07-04, 2025-07-05, 2025-07-06],
+    NoPageBreak(All(
+        Header(Weekday(Arg<Date>())),
+        Table(
+          Sort(Filter(AllGroups(),
+                      And((Stage() == MAIN_RED), (Date(StartTime()) == Arg<Date>()))),
+               StartTime()),
+          [Column("Group", GroupName()), Column("Start", FormatTime(StartTime()))]))))
