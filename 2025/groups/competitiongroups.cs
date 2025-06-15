e8adbdadf77e1446f3479346677e26643ca48ec1
Tim Reynolds <tim@cubingusa.org>
2025-06-15 14:03:56 -0400

    Mark competitiongroups assignments as volunteering.

--- 2025/groups/competitiongroups.cs
+++ 2025/groups/competitiongroups.cs
@@ -20,55 +20,79 @@ CreateCompetitionGroupsAssignments(
 # TODO: volunteer assignments for accommodations.
 
 CreateCompetitionGroupsAssignments(
-    [2017BRYA06], "Mirror Blocks Volunteering", 2025-07-03T09:00, 2025-07-03T10:00)
+    [2017BRYA06], "Mirror Blocks Volunteering", 2025-07-03T09:00, 2025-07-03T10:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2020OREN01], "Mirror Blocks Volunteering", 2025-07-03T09:00, 2025-07-03T11:00)
+    [2020OREN01], "Mirror Blocks Volunteering", 2025-07-03T09:00, 2025-07-03T11:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2022KHAN05], "Mirror Blocks Volunteering", 2025-07-03T10:00, 2025-07-03T12:00)
+    [2022KHAN05], "Mirror Blocks Volunteering", 2025-07-03T10:00, 2025-07-03T12:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2022STON03], "Mirror Blocks Volunteering", 2025-07-03T11:00, 2025-07-03T13:00)
+    [2022STON03], "Mirror Blocks Volunteering", 2025-07-03T11:00, 2025-07-03T13:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2014MELL03], "Mirror Blocks Volunteering", 2025-07-03T12:00, 2025-07-03T13:00)
+    [2014MELL03], "Mirror Blocks Volunteering", 2025-07-03T12:00, 2025-07-03T13:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2022LEAV01], "Mirror Blocks Volunteering", 2025-07-03T13:00, 2025-07-03T14:00)
+    [2022LEAV01], "Mirror Blocks Volunteering", 2025-07-03T13:00, 2025-07-03T14:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2020TONG02], "Mirror Blocks Volunteering", 2025-07-03T13:00, 2025-07-03T15:00)
+    [2020TONG02], "Mirror Blocks Volunteering", 2025-07-03T13:00, 2025-07-03T15:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2023VINC03], "Mirror Blocks Volunteering", 2025-07-03T14:00, 2025-07-03T16:00)
+    [2023VINC03], "Mirror Blocks Volunteering", 2025-07-03T14:00, 2025-07-03T16:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2003HARD01], "Mirror Blocks Volunteering", 2025-07-03T15:00, 2025-07-03T17:00)
+    [2003HARD01], "Mirror Blocks Volunteering", 2025-07-03T15:00, 2025-07-03T17:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2015MACD03], "Mirror Blocks Volunteering", 2025-07-03T16:00, 2025-07-03T18:00)
+    [2015MACD03], "Mirror Blocks Volunteering", 2025-07-03T16:00, 2025-07-03T18:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2014MELL03], "Mirror Blocks Volunteering", 2025-07-03T17:00, 2025-07-03T18:00)
+    [2014MELL03], "Mirror Blocks Volunteering", 2025-07-03T17:00, 2025-07-03T18:00, isVolunteering=true)
 
 CreateCompetitionGroupsAssignments(
-    [2003HARD01], "FTO Volunteering", 2025-07-04T09:00, 2025-07-04T10:00)
+    [2003HARD01], "FTO Volunteering", 2025-07-04T09:00, 2025-07-04T10:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2015MACD03], "FTO Volunteering", 2025-07-04T09:00, 2025-07-04T11:00)
+    [2015MACD03], "FTO Volunteering", 2025-07-04T09:00, 2025-07-04T11:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2019BLAC02], "FTO Volunteering", 2025-07-04T10:00, 2025-07-04T12:00)
+    [2019BLAC02], "FTO Volunteering", 2025-07-04T10:00, 2025-07-04T12:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2022LEAV01], "FTO Volunteering", 2025-07-04T11:00, 2025-07-04T13:00)
+    [2022LEAV01], "FTO Volunteering", 2025-07-04T11:00, 2025-07-04T13:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2014MELL03], "FTO Volunteering", 2025-07-04T12:00, 2025-07-04T13:00)
+    [2014MELL03], "FTO Volunteering", 2025-07-04T12:00, 2025-07-04T13:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2017BRYA06], "FTO Volunteering", 2025-07-04T13:00, 2025-07-04T14:30)
+    [2017BRYA06], "FTO Volunteering", 2025-07-04T13:00, 2025-07-04T14:30, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2017CAST41], "FTO Volunteering", 2025-07-04T13:00, 2025-07-04T15:00)
+    [2017CAST41], "FTO Volunteering", 2025-07-04T13:00, 2025-07-04T15:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2022TIND01], "FTO Volunteering", 2025-07-04T14:30, 2025-07-04T16:00)
+    [2022TIND01], "FTO Volunteering", 2025-07-04T14:30, 2025-07-04T16:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2017OSUD01], "FTO Volunteering", 2025-07-04T15:00, 2025-07-04T17:00)
+    [2017OSUD01], "FTO Volunteering", 2025-07-04T15:00, 2025-07-04T17:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2017LAMB06], "FTO Volunteering", 2025-07-04T16:00, 2025-07-04T18:00)
+    [2017LAMB06], "FTO Volunteering", 2025-07-04T16:00, 2025-07-04T18:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    [2003HARD01], "FTO Volunteering", 2025-07-04T17:00, 2025-07-04T18:00)
+    [2003HARD01], "FTO Volunteering", 2025-07-04T17:00, 2025-07-04T18:00, isVolunteering=true)
 
 CreateCompetitionGroupsAssignments(
-    Persons(BooleanProperty(MULTI_VOLUNTEER)), "Multi Blind Scrambling", 2025-07-03T10:00, 2025-07-03T12:00)
+    [2023GRAY10], "Team BLD Volunteering", 2025-07-05T09:00, 2025-07-05T10:00, isVolunteering=true)
 CreateCompetitionGroupsAssignments(
-    Persons(BooleanProperty(MULTI_VOLUNTEER)), "Multi Blind Scrambling", 2025-07-05T10:00, 2025-07-05T12:00)
+    [2022TIND01], "Team BLD Volunteering", 2025-07-05T09:00, 2025-07-05T11:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2012KALH01], "Team BLD Volunteering", 2025-07-05T10:00, 2025-07-05T12:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2024BASS05], "Team BLD Volunteering", 2025-07-05T11:00, 2025-07-05T13:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2023GRAY10], "Team BLD Volunteering", 2025-07-05T12:00, 2025-07-05T13:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2022LEAV01], "Team BLD Volunteering", 2025-07-05T13:00, 2025-07-05T14:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2023MAKA02], "Team BLD Volunteering", 2025-07-05T13:00, 2025-07-05T15:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2017CAST41], "Team BLD Volunteering", 2025-07-05T14:00, 2025-07-05T16:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2008ZAFR01], "Team BLD Volunteering", 2025-07-05T15:00, 2025-07-05T17:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2017LAMB06], "Team BLD Volunteering", 2025-07-05T16:00, 2025-07-05T18:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    [2015MACD03], "Team BLD Volunteering", 2025-07-05T17:00, 2025-07-05T18:00, isVolunteering=true)
+
+
+CreateCompetitionGroupsAssignments(
+    Persons(BooleanProperty(MULTI_VOLUNTEER)), "Multi Blind Scrambling", 2025-07-03T10:00, 2025-07-03T12:00, isVolunteering=true)
+CreateCompetitionGroupsAssignments(
+    Persons(BooleanProperty(MULTI_VOLUNTEER)), "Multi Blind Scrambling", 2025-07-05T10:00, 2025-07-05T12:00, isVolunteering=true)
 
 CreateCompetitionGroupsAssignments(
     Persons(BooleanProperty(CORE_VOLUNTEER)), "2x2 Volunteer Assignments (TBD)", 2025-07-04T09:50, 2025-07-04T12:15)
