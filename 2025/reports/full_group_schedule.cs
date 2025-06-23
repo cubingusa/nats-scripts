e0b53593cbbb6124f7b6891273d95f1f5b9fb510
Tim Reynolds <tim@cubingusa.org>
2025-06-23 12:29:22 -0400

    Also output a ballroom schedule.

--- 2025/reports/full_group_schedule.cs
+++ 2025/reports/full_group_schedule.cs
@@ -9,3 +9,13 @@ Map(
                       And((Stage() == MAIN_RED), (Date(StartTime()) == Arg<Date>()))),
                StartTime()),
           [Column("Group", GroupName()), Column("Start", FormatTime(StartTime()))]))))
+
+Map(
+    [2025-07-03, 2025-07-04, 2025-07-05, 2025-07-06],
+    NoPageBreak(All(
+        Header(Weekday(Arg<Date>())),
+        Table(
+          Sort(Filter(AllGroups(),
+                      And((Stage() == SIDE_RED), (Date(StartTime()) == Arg<Date>()))),
+               StartTime()),
+          [Column("Group", GroupName()), Column("Start", FormatTime(StartTime()))]))))
