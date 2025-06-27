537bff32a92bbb1369d679d8ac0851bd3659c0f3
Tim Reynolds <tim@cubingusa.org>
2025-06-26 21:33:05 -0400

    Updates to competitiongroups.

--- 2025/groups/competitiongroups/main.cs
+++ 2025/groups/competitiongroups/main.cs
@@ -3,11 +3,10 @@
 ClearCompetitionGroupsAssignments(Persons(true))
 
 #include "accommodations.cs"
+#include "autographs.cs"
 #include "interviews.cs"
 #include "unofficial_events.cs"
 #include "../../volunteers/competitiongroups/long_events.cs"
 #include "../../volunteers/competitiongroups/tbd.cs"
 #include "../../volunteers/competitiongroups/unofficial.cs"
 #include "../../volunteers/competitiongroups/accommodations.cs"
-
-# TODO: add autographs
